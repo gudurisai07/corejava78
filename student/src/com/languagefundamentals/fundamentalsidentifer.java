c55326e340ae886a447e381f0750c432768869f0
sai <gudurisai186@gmail.com>
2026-09-03 11:02:08 +0530

    fundamentals  identifers

---
+++ student/src/com/languagefundamentals/fundamentalsidentifer.java
@@ -0,0 +1,23 @@
+package com.languagefundamentals;
+
+public class fundamentalsidentifer {
+	int rollno;
+	String name;
+	void read() {
+		System.out.println("satya");
+	}
+	void write() {
+		System.out.println("sai");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fundamentalsidentifer f = new fundamentalsidentifer();
+		 
+		System.out.println("welcome to vcube");
+		f.read();
+		f.write();
+	
+	}
+
+}
